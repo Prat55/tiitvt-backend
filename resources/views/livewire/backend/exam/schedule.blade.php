46c43f3e694a55d1eeb75c837c0ada0fcac80343
prat55 <pratikdesai9059@gmail.com>
2025-09-06 01:18:08 +0000

    Remove Duration Info Section from Exam Schedule View for Cleaner UI

--- resources/views/livewire/backend/exam/schedule.blade.php
+++ resources/views/livewire/backend/exam/schedule.blade.php
@@ -231,19 +231,6 @@ new class extends Component {
                 </div>
             </div>
 
-            <!-- Duration Info -->
-            <div class="alert alert-info">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd"
-                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                <span class="text-sm">
-                    Duration must be between 15 minutes and 5 hours. End time will be automatically
-                    calculated based on start time and duration.
-                </span>
-            </div>
-
             <!-- Form Actions -->
             <div class="flex justify-end gap-3 pt-6 border-t">
                 <x-button label="Cancel" icon="o-x-mark" class="btn-error btn-soft btn-sm"
