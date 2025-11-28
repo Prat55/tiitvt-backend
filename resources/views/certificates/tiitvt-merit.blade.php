47c79272fae5ff64c9ed12daca1a6770f624513d
prat55 <pratikdesai9059@gmail.com>
2025-11-28 15:01:35 +0000

    Adjust Certificate Template Layout for Improved Alignment
    - Modified padding and margin values in the certificate template to enhance the layout and ensure better alignment of student percentage and grade displays.

--- resources/views/certificates/tiitvt-merit.blade.php
+++ resources/views/certificates/tiitvt-merit.blade.php
@@ -165,10 +165,10 @@
     <p style="padding-top: 8pt;text-indent: 0pt;text-align: center;"><br /></p>
     <p style="text-indent: 0pt;text-align: center;">
         {{ $student->course->name ?? '-' }}</p>
-    <p style="padding-top: 6pt;padding-left: 145pt;text-indent: 0pt;text-align: left;">
+    <p style="padding-top: 6pt;padding-left: 150pt;text-indent: 0pt;text-align: left;">
         <span>{{ $student->percentage ?? '-' }}</span>
 
-        <span style="margin-left: 350px;">{{ $student->grade ?? '-' }}</span>
+        <span style="margin-left: 320px;">{{ $student->grade ?? '-' }}</span>
     </p>
 
     <p style="padding-top: 7pt;text-indent: 0pt;text-align: left;"><br /></p>
