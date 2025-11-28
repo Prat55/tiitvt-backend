eb6159cca786c4ee79924cb539c75aad87198e7a
prat55 <pratikdesai9059@gmail.com>
2025-11-28 15:29:15 +0000

    Refine Padding in Certificate Templates for Consistent Layout Alignment

--- resources/views/certificates/tiiitvt-merit-external.blade.php
+++ resources/views/certificates/tiiitvt-merit-external.blade.php
@@ -164,7 +164,7 @@
     <p style="padding-top: 8pt;text-indent: 0pt;text-align: center;"><br /></p>
     <p style="text-indent: 0pt;text-align: center;">
         {{ $student->course->name ?? 'Diploma In Full-Stack Web Development (1 year)' }}</p>
-    <p style="padding-top: 6pt;padding-left: 145pt;text-indent: 0pt;text-align: left;">
+    <p style="padding-top: 6pt;padding-left: 138pt;text-indent: 0pt;text-align: left;">
         <span>{{ $student->percentage ?? '88.50' }}</span>
 
         <span style="margin-left: 320px;">{{ $student->grade ?? 'A' }}</span>
